da292280f5ec7c111af59387339dd0dd2c54b1af
Joyce Yang <jcyang@hmc.edu>
2017-11-13 14:49:25 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/JavascriptLessonApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavascriptLessonApplication.class, args);
+	}
+}
